bab7a15606cab762f9c3d569ca963673dea6f44c
kmc <kmc@DESKTOP-2H1IFIP>
2024-12-11 02:15:07 +0200

    Add project files.

---
+++ Secondroject/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Secondroject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Write a program that allows the user to enter a number then print it.
+
+
+            /*
+               Console.WriteLine("Enter the Nums AS U Want");
+
+               int Nums = Convert.ToInt32(Console.ReadLine());
+
+               Console.WriteLine("The Nums You Entered is: " + Nums);
+            */
+            #endregion
+
+            #region   2-Write C# program that Assigning one value type variable  to another and modifying the value of one variable and  mention what will happen
+
+
+            /*
+           int a = 10;
+
+           int b = a;
+
+           Console.WriteLine($"Initial values: a = {a}, b = {b}");
+
+           // Modify the value of 'a'
+           a = 20;
+           Console.WriteLine($"After modification: a = {a}, b = {b}");
+            */
+            #endregion
+
+            #region 3-Write C# program that Assigning one reference type variable to another and   modifying the object through one variable and mention what will happen
+
+
+
+            /*
+                         String person1 = "Alice";
+
+                         String person2 = person1;
+
+                         // Print the initial names
+                         Console.WriteLine($"Initial names: person1.Name = {person1}, person2.Name = {person2}");
+
+                         // Modify the object through person1
+                         person1 = "Bob";
+
+                         // Print the names after modification
+                         Console.WriteLine($"After modification: person1.Name = {person1}, person2.Name = {person2}");
+              */
+            #endregion
+
+        }
+    }
+}
